f50c290fbe808f17fd9fef452c0cce3a67ff1694
Victor87Meireles <107935997+Victor87Meireles@users.noreply.github.com>
2022-06-28 22:49:22 -0300

    Add files via upload
    Teste Lista - Lista Coringa - Python.

---
+++ Lista Coringa.py
@@ -0,0 +1,14 @@
+valores = [1, 4, 2, 4, 7, 8, 8, 7, 9, 0]
+print("Alista foi criada assim: {}".format(valores))
+
+valores.reverse()
+print("A lista agora está invertida: {}".format(valores))
+
+contagem = valores.count(8)
+print("Nessa lista o número 8 aparece {}vezes".format(contagem))
+
+valores.sort()
+print("A lista agora está em ordem: {}".format(valores))
+
+
+
---
+++ Teste lista.py
@@ -0,0 +1,50 @@
+metallica = ["Kill en all", "Master of Puppets", "Ride The Lighthing", "And Justice For All"]
+
+print(metallica)
+
+print("Nova Lista")
+
+print(metallica[2])
+
+print("Nova Lista")
+for nome in metallica:
+    print(nome)
+print("Nova Lista")
+metallica.append("Black Album")
+
+print(metallica)
+print(nome)
+
+print("Nova Lista")
+metallica.append(input("Digite um album do Metallica"))
+for nome in metallica:
+    print(nome)
+print("Nova Lista")
+metallica.insert(3, "Garage Days")
+for nome in metallica:
+    print(nome)
+print("Nova Lista")
+
+metallica.pop()
+
+for nome in metallica:
+    print(nome)
+
+print("Nova Lista")
+
+metallica.pop(2)
+for nome in metallica:
+    print(nome)
+
+print("Nova Lista")
+
+metallica.remove("Black Album")
+
+for nome in metallica:
+    print(nome)
+
+
+
+
+
+
